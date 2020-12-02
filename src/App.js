87712f70b1a68d63bf57cc190e5db9ff3f6b11d2
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-02 15:31:02 +0400

    Added authentication check

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
 export default class App extends Component {
   render() {
-    return <div className="App" />
+    if (sessionStorage.getItem("loggedIn") === "true") {
+      return <Redirect to="/browse" />;
+    } else if (localStorage.getItem("loggedIn") === "true") {
+      sessionStorage.setItem("loggedIn", true);
+      return <Redirect to="/browse" />;
+    } else {
+      return <Redirect to="login" />;
+    }
   }
 }
