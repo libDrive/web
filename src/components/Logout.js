f3ac40c21c2ce0d6c6fa27627bf8dae4adffc6a9
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-02 15:37:58 +0400

    Added logout functionallity

---
+++ src/components/Logout.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+export default class Logout extends Component {
+  render() {
+    localStorage.clear();
+    sessionStorage.clear();
+    return <Redirect to="/" />;
+  }
+}
