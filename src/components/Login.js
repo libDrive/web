b16416b019d328cfd0e4f2b2fdb70fffb2e1b1f0
Elias Benbourenane <54410649+eliasbenb@users.noreply.github.com>
2020-12-02 15:30:12 +0400

    Created Login skeleton

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import LoginForm from "./LoginForm";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: sessionStorage.getItem("loggedIn") };
+  }
+  render() {
+    const { loggedIn } = this.state;
+    if (loggedIn === "true") {
+      return <Redirect to="/browse" />;
+    } else {
+      return <LoginForm />;
+    }
+  }
+}
