09c1f09af5758b10d45d5b343dd8f67597f4da1f
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-02 15:29:41 +0400

    Added Login and Browse routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import App from "./App";
+import Browse from "./components/Browse";
+import Login from "./components/Login";
 import "./index.css";
 
 const history = createBrowserHistory();
@@ -11,6 +13,8 @@ ReactDOM.render(
   <Router history={history}>
     <BrowserRouter>
       <Route exact path="/" component={App} />
+      <Route exact path="/browse" component={Browse} />
+      <Route exact path="/login" component={Login} />
     </BrowserRouter>
   </Router>,
   document.getElementById("root")
