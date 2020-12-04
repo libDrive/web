59bbca03c0db59641e308a1d719d744d7a20b5b1
Elias Benbourenane <54410649+eliasbenb@users.noreply.github.com>
2020-12-04 20:42:12 +0400

    Shortened code

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { Redirect } from "react-router-dom";
 
 export default class App extends Component {
   render() {
-    if (sessionStorage.getItem("loggedIn") === "true") {
-      return <Redirect to="/browse" />;
-    } else if (localStorage.getItem("loggedIn") === "true") {
-      sessionStorage.setItem("loggedIn", true);
+    if (sessionStorage.getItem("loggedIn") === "true" || localStorage.getItem("loggedIn") === "true") {
       return <Redirect to="/browse" />;
     } else {
       return <Redirect to="login" />;
