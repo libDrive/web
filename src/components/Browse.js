0bb58af81892380657412cc4dafeda58a1ba0e83
Elias Benbourenane <54410649+eliasbenb@users.noreply.github.com>
2020-12-04 20:52:56 +0400

    Included the navbar in /browse

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import Nav from "./Nav";
 
 export default class Browse extends Component {
   render() {
-    return <div className="Browse"></div>;
+    return (
+      <div>
+        <Nav />
+      </div>
+    );
   }
 }
