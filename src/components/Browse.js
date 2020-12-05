7a7a93e700235802672f7dac3cf7efe03bd90db6
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-05 19:51:51 +0400

    Integrated the 'Gallery' component into the 'Browse' page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Nav from "./Nav";
+import Gallery from "./Gallery";
 
 export class Browse extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <Gallery />
       </div>
     );
   }
