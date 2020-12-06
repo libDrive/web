b69203d4ba81be3bba20c5f03d21872fa09958f7
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-06 13:05:50 +0400

    Created skeleton code for View.js

---
+++ src/components/View.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export default class View extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      server:
+        sessionStorage.getItem("server") || localStorage.getItem("server"),
+      auth: sessionStorage.getItem("auth") || localStorage.getItem("auth"),
+      isLoaded: false,
+      metadata: {},
+      id: this.props.match.params.id,
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `${this.state.server}/api/v1/metadata?a=${this.state.auth}&id=${this.state.id}`
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          metadata: data,
+          isLoaded: true,
+        })
+      );
+  }
+
+  render() {
+    return this.state.isLoaded ? (
+      <div className="View">
+        <div></div>
+      </div>
+    ) : (
+      <div></div>
+    );
+  }
+}
