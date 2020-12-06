f0a38afc8a7b4db01b27e4c99912b595896a9f62
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-06 13:04:08 +0400

    Created browse page
    All works, I just need to add the page buttons

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import Nav from "./Nav";
-import Gallery from "./Gallery";
 
-export class Browse extends Component {
+import { Gallery, Nav } from "../components";
+
+export default class Browse extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      server:
+        sessionStorage.getItem("server") || localStorage.getItem("server"),
+      auth: sessionStorage.getItem("auth") || localStorage.getItem("auth"),
+      isLoaded: false,
+      metadata: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `${this.state.server}/api/v1/metadata?a=${this.state.auth}&r=0:16&s=popularity-des`
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          metadata: data,
+          isLoaded: true,
+        })
+      );
+  }
+
   render() {
-    return (
-      <div>
+    return this.state.isLoaded ? (
+      <div className="Browse">
         <Nav />
-        <Gallery />
+        <Gallery metadata={this.state.metadata} />
       </div>
+    ) : (
+      <div></div>
     );
   }
 }
-
-export default withRouter(Browse);
