ce01889156560366f62723e1b2f67160abdd5b77
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-15 18:22:42 +0400

    Completed view page

--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 
+import { Typography } from "@material-ui/core";
+
+import Plyr from "plyr-react";
+import "plyr-react/dist/plyr.css";
+
+import { Nav } from "../components";
+import "./View.css";
+
 export default class View extends Component {
   constructor(props) {
     super(props);
@@ -14,9 +22,9 @@ export default class View extends Component {
   }
 
   componentDidMount() {
-    fetch(
-      `${this.state.server}/api/v1/metadata?a=${this.state.auth}&id=${this.state.id}`
-    )
+    let { auth, id, server } = this.state;
+
+    fetch(`${server}/api/v1/metadata?a=${auth}&id=${id}`)
       .then((response) => response.json())
       .then((data) =>
         this.setState({
@@ -27,10 +35,35 @@ export default class View extends Component {
   }
 
   render() {
-    let { auth, id, isLoaded, metadata, server } = this.state
+    let { auth, id, isLoaded, metadata, server } = this.state;
 
-    return this.state.isLoaded ? (
+    return isLoaded ? (
       <div className="View">
+        <Nav />
+        <div className="plyr__component">
+          <Plyr
+            source={{
+              type: "video",
+              sources: [
+                {
+                  src: `${server}/api/v1/download?a=${auth}&id=${id}`,
+                },
+              ],
+            }}
+          />
+        </div>
+        <div className="info__container">
+          <div className="info__left">
+            <img className="info__poster" src={metadata.posterPath} />
+          </div>
+          <div className="info__right">
+            <Typography variant="h2">{metadata.title}</Typography>
+            <Typography variant="h6">({metadata.releaseDate})</Typography>
+            <Typography variant="body1" style={{ marginTop: "30px" }}>
+              {metadata.overview}
+            </Typography>
+          </div>
+        </div>
       </div>
     ) : (
       <div></div>
