dbf0aadecbc8e83c8b85f2a49f62458a588379a9
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-12-21 13:23:41 +0400

    Error catching improvement

--- src/pages/Settings/SettingsLoginForm.js
+++ src/pages/Settings/SettingsLoginForm.js
@@ -71,7 +71,7 @@ class SettingsLoginForm extends Component {
       })
       .catch((error) => {
         if (error.response) {
-          if (error.response.status === 401) {
+          if (error.response.status === 401 || 404) {
             alert("Your credentials are invalid");
           } else {
             alert("Something went wrong while communicating with the backend");
