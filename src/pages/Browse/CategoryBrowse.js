bc078701e2ef7b7954814e3556337cbaec2f038a
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-02-16 16:43:40 +0400

    Added sort menu for browse page

--- src/pages/Browse/CategoryBrowse.js
+++ src/pages/Browse/CategoryBrowse.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
+import { Button, Menu, MenuItem } from "@material-ui/core";
+
 import ClipLoader from "react-spinners/ClipLoader";
 
-import Swal from 'sweetalert2/src/sweetalert2.js'
+import Swal from "sweetalert2/src/sweetalert2.js";
 import "@sweetalert2/theme-dark/dark.css";
 
 import axios from "axios";
 import queryString from "query-string";
 
-import { Footer, Gallery, Nav, PageMenu } from "../../components";
+import { Footer, Gallery, Nav, PageMenu, uuid } from "../../components";
+import "../../components/SortMenu.css";
 
 export default class CategoryBrowse extends Component {
   constructor(props) {
@@ -32,12 +37,14 @@ export default class CategoryBrowse extends Component {
       }`,
       server:
         sessionStorage.getItem("server") || localStorage.getItem("server"),
+      sort:
+        queryString.parse(this.props.location.search).sort || "alphabet-asc",
     };
   }
 
   async componentDidMount() {
-    let { auth, category, range, server } = this.state;
-    let url = `${server}/api/v1/metadata?a=${auth}&c=${category}&r=${range}&s=alphabet-asc`;
+    let { auth, category, range, server, sort } = this.state;
+    let url = `${server}/api/v1/metadata?a=${auth}&c=${category}&r=${range}&s=${sort}`;
 
     axios
       .get(url)
@@ -108,6 +115,7 @@ export default class CategoryBrowse extends Component {
         <Nav />
         <Gallery metadata={metadata} />
         <PageMenu props={{ page: page, pages: pages, thisprops: this.props }} />
+        <SortMenu props={{ thisprops: this.props }} />
         <Footer />
       </div>
     ) : (
@@ -116,4 +124,152 @@ export default class CategoryBrowse extends Component {
       </div>
     );
   }
+}
+
+export function SortMenu(props) {
+  const [menuAnchorEl, setMenuAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setMenuAnchorEl(null);
+  };
+
+  let { thisprops } = props.props;
+
+  return (
+    <div className="SortMenu">
+      <div className="sort__container">
+        <Button
+          aria-controls="sort-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          variant="outlined"
+          color="primary"
+        >
+          Sort
+        </Button>
+        <Menu
+          id="sort-menu"
+          anchorEl={menuAnchorEl}
+          keepMounted
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          transformOrigin={{ vertical: "top", horizontal: "center" }}
+          open={Boolean(menuAnchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=alphabet-asc`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Alphabet Ascending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=alphabet-des`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Alphabet Descending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=date-asc`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Date Ascending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=date-des`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Date Descending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=popularity-asc`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Popularity Ascending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=popularity-des`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Popularity Descending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=vote-asc`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Vote Ascending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=vote-des`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Vote Descending
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link
+              to={{
+                pathname: thisprops.location.pathname,
+                search: `?sort=random`,
+              }}
+              key={uuid()}
+              className="no_decoration_link"
+            >
+              Random
+            </Link>
+          </MenuItem>
+        </Menu>
+      </div>
+    </div>
+  );
 }
