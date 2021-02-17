72da76256576b1782961848cb649d21d16af0c9d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-02-17 22:06:34 +0400

    Removed unnecessary imports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { fade, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 import "./Footer.css";
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import {
   AppBar,
