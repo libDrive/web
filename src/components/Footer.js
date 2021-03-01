b4dad4974198bdb4538c832c0658cfe245b7c026
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-03-01 13:27:55 +0400

    Bump to v1.1.6

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,7 +51,7 @@ export default function Footer() {
         href="https://eliasbenb.github.io"
         target="_blank"
       >
-        © 2021 Copyright: Elias Benbourenane - v1.1.5
+        © 2021 Copyright: Elias Benbourenane - v1.1.6
       </a>
       <div className="gallery__select">
         <FormControl className={classes.formControl}>
