9b8d8621d9c29607356ef44ff043b8b25164e0d3
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-03-03 10:41:56 +0400

    Removed aync loading which is reported to cause issues

--- src/pages/View/View.js
+++ src/pages/View/View.js
@@ -32,7 +32,7 @@ export default class View extends Component {
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     let { auth, id, server } = this.state;
     let url = `${server}/api/v1/metadata?a=${auth}&id=${id}`;
 
