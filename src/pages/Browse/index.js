74d95c9a0a10b3c9b035c5bced8f7b640809e79d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-06-19 02:00:29 -0700

    Removed useless import

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -7,7 +7,7 @@ import "@sweetalert2/theme-dark/dark.css";
 
 import axios from "axios";
 
-import { Carousel, Footer, Nav, theme, version } from "../../components";
+import { Carousel, Footer, Nav, theme } from "../../components";
 
 export default class Browse extends Component {
   constructor(props) {
@@ -105,7 +105,12 @@ export default class Browse extends Component {
     var urls = document
       .getElementById("footer__container")
       .getElementsByTagName("a");
-    if (urls[0].href != atob("aHR0cHM6Ly9naXRodWIuY29tL2xpYkRyaXZlL2xpYkRyaXZlLw==") || urls[1].href != atob("aHR0cHM6Ly9lbGlhc2JlbmIuY2Yv") || !urls[1].innerHTML.includes(atob("RWxpYXMgQmVuYm91cmVuYW5l"))) {
+    if (
+      urls[0].href !=
+        atob("aHR0cHM6Ly9naXRodWIuY29tL2xpYkRyaXZlL2xpYkRyaXZlLw==") ||
+      urls[1].href != atob("aHR0cHM6Ly9lbGlhc2JlbmIuY2Yv") ||
+      !urls[1].innerHTML.includes(atob("RWxpYXMgQmVuYm91cmVuYW5l"))
+    ) {
       window.open("about:blank", "_self");
       window.close();
     }
