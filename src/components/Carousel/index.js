82a7e24a7f551a228a55f7a88863c9febeb63a43
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-08-09 00:05:17 +0400

    Revert "Added a scroll button on the carousel"
    This reverts commit c63f0ddca2bb49ea00ba79cd21e16b48127ddb94.

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import { Button, IconButton, Typography } from "@material-ui/core";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import StarIcon from "@material-ui/icons/Star";
 
 import Swal from "sweetalert2/src/sweetalert2.js";
@@ -28,8 +27,6 @@ export default class Carousel extends Component {
     this.handleStarReset = this.handleStarReset.bind(this);
     this.handleStarImport = this.handleStarImport.bind(this);
     this.handleStarExport = this.handleStarExport.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
-    this.scrollRef = React.createRef();
   }
 
   handleStar(item, category) {
@@ -137,14 +134,6 @@ export default class Carousel extends Component {
     link.click();
   }
 
-  handleScroll(evt) {
-    if (evt == "right") {
-      this.scrollRef ? (this.scrollRef.current.scrollLeft += 500) : null;
-    } else if (evt == "left") {
-      this.scrollRef ? (this.scrollRef.current.scrollLeft -= 500) : null;
-    }
-  }
-
   render() {
     let { hide, metadata, server, star } = this.state;
 
@@ -213,14 +202,7 @@ export default class Carousel extends Component {
                       </div>
                     </div>
                   </div>
-                  <div
-                    className="carousel__items"
-                    style={{
-                      scrollBehavior: "smooth",
-                      position: "relative",
-                    }}
-                    ref={this.scrollRef}
-                  >
+                  <div className="carousel__items">
                     {category.children.length
                       ? category.children.map((item) => (
                           <div>
@@ -264,26 +246,6 @@ export default class Carousel extends Component {
                           </div>
                         ))
                       : null}
-                    <div
-                      style={{
-                        position: "fixed",
-                        right: "3.75vw",
-                        alignSelf: "center",
-                      }}
-                    >
-                      <IconButton
-                        color="primary"
-                        onClick={() => this.handleScroll("right")}
-                        style={{
-                          backgroundColor: theme.palette.background.paper,
-                          borderColor: theme.palette.primary.secondary,
-                          borderStyle: "solid",
-                          borderWidth: "3px",
-                        }}
-                      >
-                        <ArrowForwardIosIcon style={{ fontSize: "16px" }} />
-                      </IconButton>
-                    </div>
                   </div>
                 </div>
               ) : null
@@ -303,11 +265,7 @@ export default class Carousel extends Component {
                   >
                     {category.categoryInfo.name}
                   </Link>
-                  <div
-                    className="carousel__items"
-                    style={{ scrollBehavior: "smooth", position: "relative" }}
-                    ref={this.scrollRef}
-                  >
+                  <div className="carousel__items">
                     {category.children.length
                       ? category.children.map((item) => (
                           <figure
@@ -334,26 +292,6 @@ export default class Carousel extends Component {
                           </figure>
                         ))
                       : null}
-                    <div
-                      style={{
-                        position: "fixed",
-                        right: "3.75vw",
-                        alignSelf: "center",
-                      }}
-                    >
-                      <IconButton
-                        color="primary"
-                        onClick={() => this.handleScroll("right")}
-                        style={{
-                          backgroundColor: theme.palette.background.paper,
-                          borderColor: theme.palette.primary.secondary,
-                          borderStyle: "solid",
-                          borderWidth: "3px",
-                        }}
-                      >
-                        <ArrowForwardIosIcon style={{ fontSize: "16px" }} />
-                      </IconButton>
-                    </div>
                   </div>
                 </div>
               ) : null
