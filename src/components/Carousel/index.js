106fe42d8bc1c225538cdb1de266c384fe91c03b
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-08-12 23:05:32 +0400

    Replaced the pinned icon with an actual pin

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -6,8 +6,6 @@ import { IconButton, Tooltip, Typography } from "@material-ui/core";
 import CloudDownloadIcon from "@material-ui/icons/CloudDownload";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import DeleteIcon from "@material-ui/icons/Delete";
-import RoomIcon from "@material-ui/icons/Room";
-import RoomOutlinedIcon from "@material-ui/icons/RoomOutlined";
 import StarIcon from "@material-ui/icons/Star";
 
 import Swal from "sweetalert2/src/sweetalert2.js";
@@ -250,7 +248,24 @@ export default class Carousel extends Component {
                               color="primary"
                               onClick={() => this.handlePin(p, false)}
                             >
-                              <RoomIcon />
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                enableBackground="new 0 0 24 24"
+                                height="24px"
+                                viewBox="0 0 24 24"
+                                width="24px"
+                                fill={theme.palette.primary.main}
+                              >
+                                <g>
+                                  <rect fill="none" height="24" width="24" />
+                                </g>
+                                <g>
+                                  <path
+                                    d="M16,9V4l1,0c0.55,0,1-0.45,1-1v0c0-0.55-0.45-1-1-1H7C6.45,2,6,2.45,6,3v0 c0,0.55,0.45,1,1,1l1,0v5c0,1.66-1.34,3-3,3h0v2h5.97v7l1,1l1-1v-7H19v-2h0C17.34,12,16,10.66,16,9z"
+                                    fillRule="evenodd"
+                                  />
+                                </g>
+                              </svg>
                             </IconButton>
                           </Tooltip>
                         ) : (
@@ -259,7 +274,21 @@ export default class Carousel extends Component {
                               color="primary"
                               onClick={() => this.handlePin(p, true)}
                             >
-                              <RoomOutlinedIcon />
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                enableBackground="new 0 0 24 24"
+                                height="24px"
+                                viewBox="0 0 24 24"
+                                width="24px"
+                                fill={theme.palette.primary.main}
+                              >
+                                <g>
+                                  <rect fill="none" height="24" width="24" />
+                                </g>
+                                <g>
+                                  <path d="M14,4v5c0,1.12,0.37,2.16,1,3H9c0.65-0.86,1-1.9,1-3V4H14 M17,2H7C6.45,2,6,2.45,6,3c0,0.55,0.45,1,1,1c0,0,0,0,0,0l1,0v5 c0,1.66-1.34,3-3,3v2h5.97v7l1,1l1-1v-7H19v-2c0,0,0,0,0,0c-1.66,0-3-1.34-3-3V4l1,0c0,0,0,0,0,0c0.55,0,1-0.45,1-1 C18,2.45,17.55,2,17,2L17,2z" />
+                                </g>
+                              </svg>
                             </IconButton>
                           </Tooltip>
                         )}
@@ -285,10 +314,7 @@ export default class Carousel extends Component {
                                   alt={item.title}
                                 />
                               </Link>
-                              <Typography
-                                className="carousel__item__title"
-                                key={guid()}
-                              >
+                              <Typography className="carousel__item__title">
                                 {item.title}
                               </Typography>
                               <div
